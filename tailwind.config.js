357ec55fd8ea5f054ff13be6c7a81c4858f74054
dipenmotka <dipen@atharvasystem.com>
2022-12-13 19:11:16 +0530

    tailwind added using tailwind cli  and postcss cli

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './static/**/*.js',
+    './views/**/*.pug',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
